fc33af6aeaae13c9cedaed2ea2741c20f0e244c6
Markus Beamer <MarkusB@rtriad.pro>
2020-02-11 16:49:28 -0500

    init

---
+++ 20-speech-to-text-azure/Test/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+
+namespace Test
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            await RecognizeSpeechAsync();
+        }
+
+        static async Task RecognizeSpeechAsync()
+        {
+            //var config = SpeechConfig.FromSubscription("YourSubscriptionKey", "YourServiceRegion");
+            var config = SpeechConfig.FromSubscription("0fb0fc6966b64602831607f160118010", "eastus2");
+
+            using (var audioInput = AudioConfig.FromWavFileInput("whatstheweatherlike.wav"))
+
+            using (var recognizer = new SpeechRecognizer(config, audioInput))
+            {
+                Console.WriteLine("Recognizing first result...");
+                var result = await recognizer.RecognizeOnceAsync();
+
+                switch (result.Reason)
+                {
+                    case ResultReason.RecognizedSpeech:
+                           Console.WriteLine($"We recognized: {result.Text}");
+                            //System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+
+                            break;
+                    case ResultReason.NoMatch:
+                        Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                        break;
+                    case ResultReason.Canceled:
+                        var cancellation = CancellationDetails.FromResult(result);
+                        Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                        if (cancellation.Reason == CancellationReason.Error)
+                        {
+                            Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                            Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                            Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                        }
+                        break;
+                }
+            }
+
+
+        }
+    }
+}
